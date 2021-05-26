c990fb83f7227b195d2c09c6d67151abfffb852c
Vita Khutorianska <steblvita@gmail.com>
2021-05-26 15:10:37 +0300

    add hookk to Filter and login

--- src/Components/Filter/Filter.jsx
+++ src/Components/Filter/Filter.jsx
@@ -1,32 +1,30 @@
 import PropTypes from 'prop-types';
 import styles from './Filter.module.css';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import * as actions from '../../Redux/Phonebook/phonebook-action';
 import { getFilter } from '../../Redux/Phonebook/phonebook-selectors';
-const Filter = ({ value, onChange }) => (
-  <div className={styles.container}>
-    <label className={styles.label}>
-      Find contact by name
-      <input
-        className={styles.input}
-        type="text"
-        value={value}
-        onChange={onChange}
-      />
-    </label>
-  </div>
-);
+const Filter = () => {
+  const dispatch = useDispatch();
+  const onChange = e => dispatch(actions.filterChange(e.currentTarget.value));
+  const value = useSelector(state => getFilter(state));
+
+  return (
+    <div className={styles.container}>
+      <label className={styles.label}>
+        Find contact by name
+        <input
+          className={styles.input}
+          type="text"
+          value={value}
+          onChange={onChange}
+        />{' '}
+      </label>
+    </div>
+  );
+};
 Filter.propTypes = {
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
 };
-const mapStateToProps = (state, props) => {
-  return { value: getFilter(state) };
-};
 
-const mapDispatchToProps = dispatch => {
-  return {
-    onChange: e => dispatch(actions.filterChange(e.currentTarget.value)),
-  };
-};
-export default connect(mapStateToProps, mapDispatchToProps)(Filter);
+export default Filter;
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,77 +1,59 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import styles from './Login.module.css';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { login } from '../../Redux/Auth/auth_operation';
 
-class Login extends Component {
-  state = {
-    name: '',
-    email: '',
-    password: '',
-  };
+const Login = () => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState('ciba@gmail.com');
+  const [password, setPassword] = useState('12345qqqq');
 
-  handleChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-  };
-  handleSubmit = e => {
+  const handleChangeEmail = e => setEmail(e.target.value);
+  const handleChangePassword = e => setPassword(e.target.value);
+
+  const handleSubmit = e => {
     e.preventDefault();
+    const payload = {
+      email,
+      password,
+    };
 
-    // const sameName = this.props.items.some(
-    //   (item) => item.name === this.state.name
-    // );
-    // if (sameName) {
-    //   window.alert(
-    //     `LocalHost:3000 says ${this.state.name} is already in contact`
-    //   );
-    //   this.reset();
-    //   return;
-    // }
-    this.props.onLogin(this.state);
-    this.reset();
+    dispatch(login(payload));
+    reset();
   };
-  reset = () => {
-    return this.setState({ name: '', email: '', password: '' });
+  const reset = () => {
+    // eslint-disable-next-line no-sequences
+    return setEmail(''), setPassword('');
   };
-
-  render() {
-    const { name, email, password } = this.state;
-    return (
-      <div className={styles.form_container}>
-        <form
-          className={styles.form}
-          onSubmit={this.handleSubmit}
-          autoComplete="off"
-        >
-          <label className={styles.label}>
-            Email
-            <input
-              className={styles.input}
-              type="email"
-              name="email"
-              value={email}
-              onChange={this.handleChange}
-            />
-          </label>
-          <label className={styles.label}>
-            <span className={styles.span}>Password</span>
-            <input
-              className={styles.input}
-              type="password"
-              name="password"
-              value={password}
-              onChange={this.handleChange}
-            />
-          </label>
-          <button type="submit" className={styles.btn}>
-            Login
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
-const mapDispatchToProps = {
-  onLogin: login,
+  return (
+    <div className={styles.form_container}>
+      <form className={styles.form} onSubmit={handleSubmit} autoComplete="off">
+        <label className={styles.label}>
+          Email
+          <input
+            className={styles.input}
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleChangeEmail}
+          />
+        </label>
+        <label className={styles.label}>
+          <span className={styles.span}>Password</span>
+          <input
+            className={styles.input}
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChangePassword}
+          />
+        </label>
+        <button type="submit" className={styles.btn}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
-export default connect(null, mapDispatchToProps)(Login);
+
+export default Login;
