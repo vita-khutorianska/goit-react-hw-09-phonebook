6bdef5d97415997e4cd539af6ec562227b10a65e
Vita Khutorianska <steblvita@gmail.com>
2021-05-26 11:39:51 +0300

    add hook to contact list and contct

--- src/Components/ContactList/ContactList.jsx
+++ src/Components/ContactList/ContactList.jsx
@@ -1,30 +1,37 @@
 import styles from './ContactList.module.css';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact } from '../../Redux/Phonebook/operationsApi';
 import { getUniqContact } from '../../Redux/Phonebook/phonebook-selectors';
-const ContactList = ({ onDeleteContact, contacts }) => (
-  <div className={styles.contact}>
-    ContactList
-    <ul>
-      {contacts.map(({ name, id, number }) => {
-        return (
-          <li key={id}>
-            <span>{name}:</span>
-            <span>{number}</span>
-            <button
-              type="submit"
-              className={styles.contact_btn}
-              onClick={() => onDeleteContact(id)}
-            >
-              Delete
-            </button>
-          </li>
-        );
-      })}
-    </ul>
-  </div>
-);
+const ContactList = () => {
+  const dispatch = useDispatch();
+  const onDeleteContact = id => {
+    dispatch(deleteContact(id));
+  };
+  const contacts = useSelector(state => getUniqContact(state));
+  return (
+    <div>
+      <ul className={styles.contact_list}>
+        {contacts.map(({ name, id, number }) => {
+          return (
+            <li className={styles.contact_item} key={id}>
+              {' '}
+              <span className={styles.contact_item_name}>{name}:</span>
+              <span className={styles.contact_item_tel}>{number}</span>{' '}
+              <button
+                type="submit"
+                className={styles.contact_btn}
+                onClick={() => onDeleteContact(id)}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 ContactList.propTypes = {
   onDeleteContact: PropTypes.func.isRequired,
@@ -37,12 +44,4 @@ ContactList.propTypes = {
   ).isRequired,
 };
 
-const mapStateToProps = state => ({
-  contacts: getUniqContact(state),
-});
-const mapDispatchToProps = dispatch => {
-  return {
-    onDeleteContact: id => dispatch(deleteContact(id)),
-  };
-};
-export default connect(mapStateToProps, mapDispatchToProps)(ContactList);
+export default ContactList;
--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -1,33 +1,23 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { getContact } from '../../Redux/Phonebook/operationsApi';
-import { getLoading } from '../../Redux/Phonebook/phonebook-selectors';
+// import { getLoading } from '../../Redux/Phonebook/phonebook-selectors';
 import FormPhonebook from '../ContactForm';
 import ContactItem from '../ContactList';
 import Filter from '../Filter';
 
-class Contacts extends Component {
-  componentDidMount() {
-    this.props.getContacts();
-  }
-  render() {
-    return (
-      <div>
-        <FormPhonebook />
-        <Filter />
-        <div title="Contacts list">
-          <ContactItem />
-        </div>
+const Contacts = () => {
+  const dispatch = useDispatch();
+  useEffect(() => dispatch(getContact(), [dispatch]));
+  return (
+    <div>
+      <FormPhonebook />
+      <Filter />
+      <div title="Contacts list">
+        <ContactItem />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-const mapStateToProps = state => ({
-  isLoading: getLoading(state),
-});
-const mapDispatchToProps = dispatch => ({
-  getContacts: () => dispatch(getContact()),
-});
-export default connect(mapStateToProps, mapDispatchToProps)(Contacts);
-// export default Contacts;
+export default Contacts;
